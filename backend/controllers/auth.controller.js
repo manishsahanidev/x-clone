b0964c8cf5094d3ba9b658cb73e57e851ae15d47
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2024-06-27 10:30:21 +0530

    project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "signup endpoint hit",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    data: "login endpoint hit",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    data: "logout endpoint hit",
+  });
+};
